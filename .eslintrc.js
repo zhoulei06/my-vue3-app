5fad157ec3318ba5f2e708d584834183f7507a56
zhoulei <zhoulei@geyecloud.com>
2021-04-20 09:47:17 +0800

    提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
+/*
+ * @Description:
+ * @Author: ZHOUL
+ * @Date: 2021-04-19 20:43:21
+ * @LastEditTime: 2021-04-20 09:46:59
+ * @LastEditors:
+ */
 module.exports = {
   env: {
     browser: true,
@@ -6,6 +13,7 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     'airbnb-base',
+    'plugin:prettier/recommended' // 添加 prettier 插件
   ],
   parserOptions: {
     ecmaVersion: 12,
